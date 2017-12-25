68f01827b585ad904243a393133b03fff9b01cd0
Bret Comnes <bcomnes@gmail.com>
2017-12-24 16:14:55 -0800

    Skip spread rest on node 0.12

--- test/rest-spread.js
+++ test/rest-spread.js
@@ -4,6 +4,12 @@ var fs = require('fs');
 var src = fs.readFileSync(__dirname + '/files/rest-spread.js');
 
 test('rest-spread', function (t) {
-    t.doesNotThrow(detective.bind(detective, src), 'Files with rest or spread do not throw')
+    try { 
+        require('acorn5-object-spread/inject')
+        t.doesNotThrow(detective.bind(detective, src), 'Files with rest or spread do not throw')
+    } catch (_) {
+        // node 0.12
+        t.pass('SKIPPED rest spread')
+    }
     t.end();
 });
